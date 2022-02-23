fe430d375458b8ae41d9d436ef47300ced2fb851
KelvinWong <wongwaikit2873@gmail.com>
2022-02-23 09:39:37 +0800

    first commit

---
+++ src/Components/APIServer.js
@@ -0,0 +1,14 @@
+export default class APIService{
+    static InsertArticle(body){
+        return fetch(`http://localhost:5000/add`,{
+            'method':'POST',
+             headers : {
+            'Content-Type':'application/json'
+      },
+      body:JSON.stringify(body)
+    })
+    .then(response => response.json())
+    .catch(error => console.log(error))
+    }
+
+}
